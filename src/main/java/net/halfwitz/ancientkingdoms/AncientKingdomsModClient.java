8f1e45cf29ccc9b858b8167ae908d11ae6f94183
Michael <mlorenz1012@aol.com>
2023-07-09 09:41:41 -0400

    Initial commit

---
+++ src/main/java/net/halfwitz/ancientkingdoms/AncientKingdomsModClient.java
@@ -0,0 +1,12 @@
+package net.halfwitz.ancientkingdoms;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class AncientKingdomsModClient implements ClientModInitializer
+{
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/halfwitz/ancientkingdoms/AncientKingdomsModDataGenerator.java
@@ -0,0 +1,11 @@
+package net.halfwitz.ancientkingdoms;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class AncientKingdomsModDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
