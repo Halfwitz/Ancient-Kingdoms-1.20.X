91c8b6a4bade679dfb38ada0c8e3e57996e02d40
Michael <mlorenz1012@aol.com>
2023-07-09 09:43:32 -0400

    Reorganized Code

--- src/main/java/net/halfwitz/ancientkingdoms/AncientKingdomsMod.java
+++ src/main/java/net/halfwitz/ancientkingdoms/AncientKingdomsMod.java
@@ -6,10 +6,11 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class AncientKingdomsMod implements ModInitializer {
+
+	public static final String MOD_ID = "ancientkingdoms"; //Mod ID (must be lowercase!!)
 	// This logger is used to write text to the console and the log file.
 	// It is considered best practice to use your mod id as the logger's name.
 	// That way, it's clear which mod wrote info, warnings, and errors.
-    public static final String MOD_ID = "ancientkingdoms"; //Mod ID (must be lowercase!!)
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
 	@Override
